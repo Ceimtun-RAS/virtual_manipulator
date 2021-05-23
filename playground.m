ea5693c10302ab68c710b51d0ba763bd1ea4e477
Cristian Chitiva <cychitivav@unal.edu.co>
2021-05-22 19:38:00 -0500

    drop_item function added

--- playground.m
+++ playground.m
@@ -1,14 +1,11 @@
+clc, clear, close all
 %% Connect to ROS Network
-clc, clear 
-ipaddress = '192.168.182.128';
+%ipaddress = '192.168.182.128'; % VM
+ipaddress = '192.168.0.2';
+rosshutdown;
 
-%ipaddress = '10.0.3.25';
-
-rosshutdown
 rosinit(ipaddress,11311);
-
 %% testing ROS commands 
-
 rosaction("list")
 rostopic("info", "tf")
 rostopic("info", "clock") 
@@ -16,25 +13,21 @@ svc_list =rosservice("list");
 rosservice("info","/gazebo/set_model_configuration")
 rosservice info '/gazebo/set_model_configuration'
 clc
-
 %% load robot model
-
 load('exampleHelperKINOVAGen3GripperROSGazebo.mat');
 %loadrobot("kinovaGen3");
 %axes = show(robot);
-
 %% Initialize 
 physicsClient = rossvcclient('/gazebo/pause_physics');
-physicsResp = call(physicsClient,'Timeout',3);
+call(physicsClient,'Timeout',3);
 
 RoboCupManipulation_setInitialConfig; % DO NOT MODIFY
-%initial pos [ -1.9494   -0.0346   -1.1962   -1.0550    0.0367   -2.0500    1.5847]; 
-% "joint_1", "joint_2", "joint_3","joint_4", "joint_5", "joint_6", "joint_7"
-
+% Initial pose
+% ["joint_1" "joint_2" "joint_3" "joint_4" "joint_5" "joint_6" "joint_7"]
+% [ -1.9494   -0.0346   -1.1962   -1.0550    0.0367   -2.0500    1.5847]
 %% unpause gazebo
 physicsClient = rossvcclient('gazebo/unpause_physics');
 physicsResp = call(physicsClient,'Timeout',3);
-
 
 %% Configure ROS subscriber 
 jointSub = rossubscriber('/my_gen3/joint_states');
@@ -42,16 +35,12 @@ ros_action='/my_gen3/gen3_joint_trajectory_controller/follow_joint_trajectory';
 [trajAct,trajGoal] = rosactionclient( ros_action);
 % [client,goalMsg] = rosactionclient( actionName)
 showdetails(trajGoal)
-
 %% Define position 
-
 base_pos=[-0.13, -0.1, 0.6];
-bottle_pos=[0.26 -0.27 0.54];
-bluebin_pos=[-0.3 -0.4 0.4];
-greenbin_pos=[-0.3 0.4 0.4];
-%translation=[-0.3 0.4 0.4];
-translation=bottle_pos-base_pos; % Botella facil
-translation=bluebin_pos;
+bottle_pos=[0.26 -0.27 0.54];% Botella facil
+
+
+translation=bottle_pos-base_pos; 
 
 orientation=[pi 0 pi/2];
 H_transform=trvec2tform(translation)*eul2tform(orientation,'XYZ')  
@@ -71,10 +60,10 @@ trajGoal = packageJointTrajectory(trajGoal,q,qd,qdd,trajTimes);
 
 waitForServer(trajAct);
 sendGoal(trajAct,trajGoal)
-pause(4)
-SLActivateGripper("close")
-pause(4)
-SLActivateGripper("open")
+%waitForServer(trajAct);
+%SLActivateGripper("close")
+
+%drop_item("green")
 
 %% Plot trajectory
 close all
@@ -107,3 +96,36 @@ hold off
 % rgbImg = readImage(rgbImgSub.LatestMessage); 
 % imshow(rgbImg)
 
+
+function drop_item(bin_name)
+    if bin_name == "blue"
+        bin_pos = [-0.3 -0.4 0.4];
+    elseif bin_name == "green"
+        bin_pos = [-0.3 0.4 0.4];
+    end
+    
+    move_time = 4;
+
+    jointSub = rossubscriber('/my_gen3/joint_states');
+    ros_action='/my_gen3/gen3_joint_trajectory_controller/follow_joint_trajectory';
+    [trajAct,trajGoal] = rosactionclient( ros_action);
+    showdetails(trajGoal)
+
+    orientation = [pi 0 pi/2];
+    H_transform = trvec2tform(bin_pos)*eul2tform(orientation,'XYZ');
+
+    jointMsg = receive(jointSub,2);
+    jointPosition =  jointMsg.Position(2:8);
+
+    jointMsg.Name;
+
+    [q,qd,qdd,trajTimes] = RoboCupManipulation_computeTrajectory( ...
+                            jointPosition, H_transform, robot, 'gripper', move_time);                      
+
+    trajGoal = packageJointTrajectory(trajGoal,q,qd,qdd,trajTimes);
+
+    waitForServer(trajAct);
+    sendGoal(trajAct,trajGoal)
+    waitForServer(trajAct);
+    SLActivateGripper("open")
+end
